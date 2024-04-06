ec21e204828b13c067985895c63abf8e74c02541
vipuL2701 <vikratsethji@GMAIL.COM>
2024-04-06 11:02:15 +0530

    add des...

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const lightIcon = document.getElementById("light-icon");
 const darkIcon = document.getElementById("dark-icon");
 
-// Check if dark mode is preferred
 const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 let darkMode = darkModeMediaQuery.matches;
 
-// Set dark-mode class on body if darkMode is true and pick icon
 if (darkMode) {
   document.body.classList.add("dark-mode");
   darkIcon.setAttribute("display", "none");
@@ -13,21 +11,16 @@ if (darkMode) {
   lightIcon.setAttribute("display", "none");
 }
 
-
-// Toggle dark mode on button click
 function toggleDarkMode() {
-    // Toggle darkMode variable
-    darkMode = !darkMode;
-  
-    // Toggle dark-mode class on body
-    document.body.classList.toggle("dark-mode");
-  
-    // Toggle light and dark icons
-    if (darkMode) {
-      lightIcon.setAttribute("display", "block");
-      darkIcon.setAttribute("display", "none");
-    } else {
-      lightIcon.setAttribute("display", "none");
-      darkIcon.setAttribute("display", "block");
-    }
+  darkMode = !darkMode;
+
+  document.body.classList.toggle("dark-mode");
+
+  if (darkMode) {
+    lightIcon.setAttribute("display", "block");
+    darkIcon.setAttribute("display", "none");
+  } else {
+    lightIcon.setAttribute("display", "none");
+    darkIcon.setAttribute("display", "block");
   }
+}
