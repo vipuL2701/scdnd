f2fa02b9f180719546d09e569c3caf68f565514d
vipuL2701 <vikratsethji@GMAIL.COM>
2024-04-09 17:27:40 +0530

    login

--- script.js
+++ script.js
@@ -42,3 +42,15 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+
+// ????????????????????????LoginJS
+
+const switchers = [...document.querySelectorAll('.switcher')]
+
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		this.parentElement.classList.add('is-active')
+	})
+})
+
